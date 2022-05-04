d393024a389bfb33ddea8978c88cdae503c08210
Andrei <andrei.chernov@swishfund.nl>
2022-05-04 14:29:32 +0200

    split function

--- src/cat.py
+++ src/cat.py
@@ -2,7 +2,6 @@ import settings
 from settings import logger
 from utility import get_project_dir
 from video_editor import cat_video
-#from studio import post_video
 
 
 def main():
@@ -10,7 +9,6 @@ def main():
     project_dir = get_project_dir()
     settings.project_dir = project_dir
     cat_video()
-    #post_video(project_dir)
 
 
 if __name__ == '__main__':
